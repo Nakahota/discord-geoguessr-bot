1d6644f22200e45d5a308a2a32002cfa8085475f
なかやま <nakay5251@gmail.com>
2026-09-02 23:09:43 +0900

    【実装】問題登録処理と問題送信処理を実装。

--- src/command.ts
+++ src/command.ts
@@ -1,16 +1,100 @@
-import {
-  ChatInputCommandInteraction,
-  MessageFlags,
-  SlashCommandBuilder,
-} from "discord.js";
+import { ChatInputCommandInteraction } from "discord.js";
+import prisma from "./client";
 
-export const ephemeral = {
-  data: new SlashCommandBuilder().setName('ephemeral').setDescription('あなたにだけ見えるコメントを送信します。'),
+// 問題登録処理
+export async function registerQuestion(interaction: ChatInputCommandInteraction): Promise<void> {
 
-  async execute(interaction: ChatInputCommandInteraction) {
-    await interaction.reply({
-      content: "非公開になってますか？",
-      flags: MessageFlags.Ephemeral,
-    });
-  },
-};
+    const prefecture = interaction.options.getString("prefecture", true);
+    const city = interaction.options.getString("city", true);
+    const latitude = interaction.options.getNumber("latitude", true);
+    const longitude = interaction.options.getNumber("longitude", true);
+    const imageUrl = interaction.options.getString("image_url", true);
+
+    try {
+
+        const question = await prisma.$transaction(async (tx) => {
+
+            const location = await tx.location.create({
+                data: {
+                    prefecture,
+                    city,
+                    latitude,
+                    longitude
+                }
+            });
+
+            const image = await tx.image.create({
+                data: {
+                    url: imageUrl
+                }
+            });
+
+            return await tx.question.create({
+                data: {
+                    locationId: location.id,
+                    imageId: image.id
+                }
+            });
+
+        });
+
+        await interaction.reply({
+            content: `問題(ID: ${question.id})を登録しました。`
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        await interaction.reply({
+            content: "問題登録に失敗しました。",
+            ephemeral: true
+        });
+
+    }
+}
+
+// 問題送信処理
+export async function sendQuestion(
+    interaction: ChatInputCommandInteraction
+): Promise<void> {
+
+    try {
+
+        const question = await prisma.question.findFirst({
+            include: {
+                location: true,
+                image: true
+            },
+            orderBy: {
+                id: "asc"
+            }
+        });
+
+        if (!question) {
+
+            await interaction.reply({
+                content: "問題が登録されていません。"
+            });
+
+            return;
+        }
+
+        await interaction.reply({
+            content:
+`## 問題
+
+画像: ${question.image.url}`
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        await interaction.reply({
+            content: "問題取得に失敗しました。",
+            ephemeral: true
+        });
+
+    }
+}
